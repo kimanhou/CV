b20e68f9f3ea40f540d4422d8c31b9907e56978c
Kim Anh Nguyen <kimanhou@gmail.com>
2023-06-10 17:07:35 +0900

    🎉 Set up: copy from le-bec

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./components/Home/Home";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Contact } from "./components/Contact/Contact";
+import { Shop } from "./components/Shop/Shop";
+import { About } from "./components/About/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("poney");
+    return (
+        <div className="App">
+            <HashRouter>
+                <Header />
+                <Routes>
+                    <Route path={"/contact"} element={<Contact />}></Route>
+
+                    <Route path={"/shop"} element={<Shop />}></Route>
+
+                    <Route path={"/about"} element={<About />}></Route>
+
+                    <Route path={"/"} element={<Home />}></Route>
+                </Routes>
+                <Footer />
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
