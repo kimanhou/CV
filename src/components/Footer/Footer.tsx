494ee0c2eb712f539b3e5e6a87e832fcfe60c4fb
Kim Anh Nguyen <kimanhou@gmail.com>
2023-09-23 16:00:36 +0900

    🚑️ Footer UI

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import "./Footer.scss";
 export const Footer: React.FC = (props) => {
     return (
         <footer>
+            <div className="background" />
             <svg
                 className="triangle"
                 version="1.1"
@@ -16,19 +17,12 @@ export const Footer: React.FC = (props) => {
             >
                 <polygon points="0,480 0,0 1920,0 "></polygon>
             </svg>
-            <div className="bg">
-                <div className="bg__container">
-                    <div className="bg__wrapper"></div>
-                    <div className="bg__main">
-                        <div className="bg__container">
-                            <div className="info">
-                                <SocialBar />
-                                <p className="info__copy">
-                                    Made with &#128420; &copy; 2023
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+            <div className="bg__container">
+                <div className="info">
+                    <SocialBar />
+                    <p className="info__copy">
+                        Made with &#128420; &copy; 2023
+                    </p>
                 </div>
             </div>
         </footer>
