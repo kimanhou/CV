ee69479a95b6a59d8e2b8ebe9ea9102fd7278114
Kim Anh Nguyen <kimanhou@gmail.com>
2023-09-23 16:41:30 +0900

    💄 Fix UI

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,17 +3,20 @@ import "./Divider.scss";
 
 export const Divider: React.FC = (props) => {
     return (
-        <svg
-            id="divider"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            preserveAspectRatio="none"
-            viewBox="0 0 1920 960"
-            xmlSpace="preserve"
-        >
-            <polygon points="1920,480 0,0 1920,0"></polygon>
-            <polygon points="0,480 0,960 1920, 960"></polygon>
-        </svg>
+        <div id="divider">
+            <div id="divider-background"></div>
+            <svg
+                id="divider-svg"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                preserveAspectRatio="none"
+                viewBox="0 0 1920 960"
+                xmlSpace="preserve"
+            >
+                <polygon points="1920,480 0,0 1920,0"></polygon>
+                <polygon points="0,480 0,960 1920, 960"></polygon>
+            </svg>
+        </div>
     );
 };
