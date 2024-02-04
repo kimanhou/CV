1accd364db67208e2f0136623515609a39b600fa
Kim Anh Nguyen <kimanhou@gmail.com>
2024-02-04 17:51:19 +0900

    🎨 Refactor Intro box 4 with Animator

--- src/components/Intro/IntroBox4.tsx
+++ src/components/Intro/IntroBox4.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./IntroBox4.scss";
 import { SocialBar } from "components/Social";
+import { Animator } from "components/common/Animator/Animator";
 
 export const IntroBox4: React.FC = (props) => {
     return (
@@ -8,13 +9,12 @@ export const IntroBox4: React.FC = (props) => {
             id="intro-box-4"
             className="flex-1 intro-box flex-column justify-content-center align-items-center"
         >
-            <div
-                id="intro-box-4-content"
-                className="flex-column justify-content-center align-items-center"
-            >
+            <Animator animationClassName="intro-box-4-h1">
                 <h1>WANT TO SAY 'HELLO' ?</h1>
+            </Animator>
+            <Animator animationClassName="intro-box-4-social-bar">
                 <SocialBar />
-            </div>
+            </Animator>
         </div>
     );
 };
