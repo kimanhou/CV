4e071c2aced614b9582462509d08fd1565296855
Kim Anh Nguyen <kimanhou@gmail.com>
2024-02-11 13:57:42 +0900

    📱 Mobile version

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -4,18 +4,33 @@ import { IntroBox1 } from "./IntroBox1";
 import { IntroBox3 } from "./IntroBox3";
 import { IntroBox4 } from "./IntroBox4";
 import { IntroBox2 } from "./IntroBox2";
+import { useIsMobile } from "hooks/useIsMobile";
 
 export const Intro: React.FC = (props) => {
+    const isMobile = useIsMobile();
+
     return (
         <section id="intro" className="flex-row">
-            <div id="intro-column-left" className="flex-column">
-                <IntroBox2 />
-                <div className="flex-row flex-1">
+            {!isMobile && (
+                <>
+                    <div id="intro-column-left" className="flex-column">
+                        <IntroBox2 />
+                        <div className="flex-row flex-1">
+                            <IntroBox3 />
+                            <IntroBox4 />
+                        </div>
+                    </div>
+                    <IntroBox1 />
+                </>
+            )}
+            {isMobile && (
+                <>
+                    <IntroBox1 />
+                    <IntroBox2 />
                     <IntroBox3 />
                     <IntroBox4 />
-                </div>
-            </div>
-            <IntroBox1 />
+                </>
+            )}
         </section>
     );
 };
--- src/components/Intro/IntroBox3.tsx
+++ src/components/Intro/IntroBox3.tsx
@@ -1,19 +1,50 @@
-import React from "react";
-import "./IntroBox3.scss";
+import React, { useEffect, useRef, useState } from "react";
+import { useIsMobile } from "hooks/useIsMobile";
 import computer from "./computer.png";
 import Tag from "components/common/Tag/Tag";
 import { RAKUTEN_WORK_INFO } from "components/Main/Work/utils";
 import { Animator } from "components/common/Animator/Animator";
+import "./IntroBox3.scss";
 
 export const IntroBox3: React.FC = (props) => {
+    const h1Ref = useRef<HTMLHeadingElement>(null);
+    const [h1Height, setH1Height] = useState<number | null>(null);
+    const [h1Width, setH1Width] = useState<number | null>(null);
+
+    const isMobile = useIsMobile();
+
+    const getH1Style = () => {
+        if (isMobile && h1Height !== null && h1Width !== null) {
+            return {
+                width: `${h1Height}px`,
+                height: `${h1Width}px`,
+                transform: `translateY(${h1Width - h1Height}px)`,
+            };
+        }
+        return {};
+    };
+
+    useEffect(() => {
+        if (isMobile) {
+            const timeOut = setTimeout(() => {
+                if (h1Ref.current) {
+                    setH1Height(h1Ref.current.offsetHeight);
+                    setH1Width(h1Ref.current.offsetWidth);
+                }
+            }, 100);
+
+            return () => clearTimeout(timeOut);
+        }
+    }, [isMobile]);
+
     return (
         <div
             id="intro-box-3"
             className="flex-1 intro-box flex-row align-items-center justify-content-center"
         >
-            <div id="intro-box-3-h1-wrapper">
+            <div id="intro-box-3-h1-wrapper" style={getH1Style()}>
                 <Animator animationClassName="intro-box-3-h1">
-                    <h1>CURRENT POSITION</h1>
+                    <h1 ref={h1Ref}>CURRENT POSITION</h1>
                 </Animator>
             </div>
             <div
