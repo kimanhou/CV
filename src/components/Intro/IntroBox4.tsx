dc10b7e8ec736888fbef27b51267ad364687e299
Kim Anh Nguyen <kimanhou@gmail.com>
2025-03-14 14:48:02 +0100

    🐛 Fix IntroBox4 background

--- src/components/Intro/IntroBox4.tsx
+++ src/components/Intro/IntroBox4.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
 import { SocialBar } from "@/components/Social";
 import AnimatorOnScroll from "@/components/common/Animator/AnimatorOnScroll";
+import pattern from "@/img/pattern.svg";
 import styles from "./IntroBox4.module.scss";
 
 const IntroBox4: FC = (props) => {
     return (
         <div className={styles.introBox4}>
+            <div
+                className={styles.background}
+                style={{ backgroundImage: `url(${pattern})` }}
+            />
             <AnimatorOnScroll
                 animationClassName={styles.introBox4H1Init}
                 animationExitClassName={styles.introBox4H1Exit}
